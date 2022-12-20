100783c6ad41a4b349b3146f3d3da2f9ad19408d
Vadim <59086091+1Valerion1@users.noreply.github.com>
2022-12-20 18:06:37 +0700

    Initial commit

---
+++ WebBack_laba2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebBack_laba2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
